af9314752fa3fe24d3e54d7ff7577fec4b007467
HP <38032283+hamuelpalallos@users.noreply.github.com>
2025-02-22 01:06:36 +0800

    update

---
+++ app/pages/tickets/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+const ticketId = computed(() => route.params.id)
+</script>
+
+<template>
+  <div>
+    Ticket ID: {{ ticketId }}
+  </div>
+</template>
