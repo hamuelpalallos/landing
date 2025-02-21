6d8a4483484499d2b32cf04b2a821609838bb921
HP <38032283+hamuelpalallos@users.noreply.github.com>
2025-02-21 22:37:20 +0800

    firebase config and assetlinks.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,48 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/ui-pro',
-    '@nuxt/content'
+    '@nuxt/content',
+    'nuxt-vuefire'
   ],
 
   devtools: {
     enabled: true
   },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true
+    },
+    preset: 'firebase',
+    firebase: {
+      gen: 2,
+      httpsOptions: {
+        region: 'asia-southeast1',
+        maxInstances: 2
+      },
+      nodeVersion: '20',
+      serverFunctionName: 'app_server'
+    }
+  },
+
+   vuefire: {
+    config: {
+      apiKey: import.meta.env.FIREBASE_API_KEY,
+      authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+      projectId: import.meta.env.FIREBASE_PROJECT_ID,
+      storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: import.meta.env.FIREBASE_APP_ID,
+      measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
+    },
+    auth: {
+      enabled: true
+      // sessionCookie: true
+      // popupRedirectResolver: 'browser',
+    },
+    emulators: false
+  },
+
 
   css: ['~/assets/css/main.css'],
 
